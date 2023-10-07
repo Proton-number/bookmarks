e62b30e655bea5e8dc19cc38ac64a96becb53247
Dacron <favouradebimpe63@gmail.com>
2023-10-07 07:24:55 +0100

    initial commit

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,14 @@
+import { Box, AppBar, Typography, Button, Toolbar } from '@mui/material'
+import { motion } from 'framer-motion'
+import React from 'react'
+
+
+function Nav() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Nav
